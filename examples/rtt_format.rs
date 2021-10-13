260056c1d43bfbddd702cb1a4b003962cef34078
juvgrfunex <fstockem@gmail.com>
2021-10-13 13:52:50 +0200

    add rtt examples; use correct memory.x

---
+++ examples/rtt_format.rs
@@ -0,0 +1,19 @@
+#![no_std]
+#![no_main]
+use stm32l0xx_hal as _;
+use panic_rtt_target as _;
+use rtt_target::{rprintln, rtt_init_print};
+
+#[cortex_m_rt::entry]
+fn main() -> ! {
+    rtt_init_print!();
+    rprintln!("RTT Works!");
+
+    let mut loop_counter: u16 = 0;
+    loop {
+        rprintln!("Loop iteration {}",loop_counter);
+        loop_counter = loop_counter.wrapping_add(1);
+    }
+    
+    
+}
