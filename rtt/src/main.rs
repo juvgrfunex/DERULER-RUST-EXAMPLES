eab64d5a54bb26c2676773962ea339ec5f1a3e19
juvgrfunex <fstockem@gmail.com>
2021-10-13 19:10:12 +0200

    restructure to workspace

--- rtt/src/main.rs
+++ rtt/src/main.rs
@@ -8,10 +8,6 @@ use rtt_target::{rprintln, rtt_init_print};
 fn main() -> ! {
     rtt_init_print!();
     rprintln!("RTT Works!");
-
-    let mut loop_counter: u16 = 0;
     loop {
-        rprintln!("Loop iteration {}",loop_counter);
-        loop_counter = loop_counter.wrapping_add(1);
     }
 }
